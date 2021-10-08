5237981b4ade0ee56caab55c3a0a2afb4d712712
Camila Maia <cmaiacd@gmail.com>
2021-10-08 14:05:01 -0300

    Fix routes

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path("", views.IndexView.as_view(), name="index"),
     path("<int:pk>/", views.DetailView.as_view(), name="detail"),
     path("<int:pk>/results/", views.ResultsView.as_view(), name="results"),
-    path("<int:question_id>/vote/", views.vote, name="vote"),
+    path("<int:poll_id>/vote/", views.vote, name="vote"),
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -4,47 +4,45 @@ from django.urls import reverse
 from django.utils import timezone
 from django.views import generic
 
-from polls.models import Choice, Question
+from polls.models import Choice, Poll, Question
 
 
 class IndexView(generic.ListView):
     template_name = "polls/index.html"
-    context_object_name = "latest_question_list"
+    context_object_name = "latest_poll_list"
 
     def get_queryset(self):
         """
-        Return the published questions (not including those set to be
-        published in the future) order by DESC poll pub_date
+        Return the published polls (not including those set to be
+        published in the future) order by DESC pub_date
         """
-        return (
-            Question.objects.prefetch_related("poll")
-            .filter(poll__pub_date__lte=timezone.now())
-            .order_by("-poll__pub_date")
+        return Poll.objects.filter(pub_date__lte=timezone.now()).order_by(
+            "-pub_date"
         )
 
 
 class DetailView(generic.DetailView):
-    model = Question
+    model = Poll
     template_name = "polls/detail.html"
 
 
 class ResultsView(generic.DetailView):
-    model = Question
+    model = Poll
     template_name = "polls/results.html"
 
 
-def vote(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
+def vote(request, poll_id):
+    poll = get_object_or_404(Poll, pk=poll_id)
 
     try:
-        selected_choice = question.choices.get(pk=request.POST["choice"])
+        selected_choice = poll.question.choices.get(pk=request.POST["choice"])
     except (KeyError, Choice.DoesNotExist):
         # Redisplay the question voting form.
         return render(
             request,
             "polls/detail.html",
             {
-                "question": question,
+                "poll": poll,
                 "error_message": "You didn't select a choice.",
             },
         )
@@ -54,6 +52,4 @@ def vote(request, question_id):
         # Always return an HttpResponseRedirect after successfully dealing
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
-        return HttpResponseRedirect(
-            reverse("polls:results", args=(question.id,))
-        )
+        return HttpResponseRedirect(reverse("polls:results", args=(poll.id,)))
