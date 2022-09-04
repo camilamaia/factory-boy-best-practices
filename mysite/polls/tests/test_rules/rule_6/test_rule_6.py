7c925583d7211200cc0331cb76bb04ae9e6b0fbb
Camila Maia <cmaiacd@gmail.com>
2022-09-04 14:40:40 +0200

    Add bad example for rule 6

--- mysite/polls/tests/test_rules/rule_6/test_rule_6.py
+++ mysite/polls/tests/test_rules/rule_6/test_rule_6.py
@@ -3,6 +3,8 @@ from pytest import fixture
 from polls.models import Question
 from polls.tests.test_with_factories.factories import PollFactory
 
+# Good
+
 
 @fixture
 def english_non_premium_with_author_poll():
@@ -20,6 +22,31 @@ def test_1(english_non_premium_with_author_poll):
 
 
 def test_2(english_non_premium_with_author_poll):
+    # ...
+    pass
+
+
+# Bad
+
+
+def test_3():
+    poll = PollFactory.build(
+        question__question_text="Pepsi or Coke?",
+        question__language=Question.EN,
+        premium=False,
+        with_author=True,
+    )
+    # ...
+    pass
+
+
+def test_4():
+    poll = PollFactory.build(
+        question__question_text="Pepsi or Coke?",
+        question__language=Question.EN,
+        premium=False,
+        with_author=True,
+    )
     # ...
     pass
 
